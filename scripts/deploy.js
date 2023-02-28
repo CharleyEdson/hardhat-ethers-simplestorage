38d775e133b5a7fc85e5ee18dedee0bf0bcdf2fa
Charley <charley1135@gmail.com>
2023-02-27 20:37:36 -0800

    Feat:Successfuly deployed and verified contract to Goerli testnet.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
 // imports
-const { ethers, run } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 // async main
 async function main() {
@@ -8,6 +8,19 @@ async function main() {
   const simpleStorage = await SimpleStorageFactory.deploy();
   await simpleStorage.deployed();
   console.log(`Deployed contract to: ${simpleStorage.address}`);
+  console.log(network.config)
+  if(network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) { 
+    await simpleStorage.deployTransaction.wait(6)
+    await verify(simpleStorage.address, [])
+  }
+
+  const currentValue = await simpleStorage.retrieve()
+  console.log(`Current Value is: ${currentValue}`)
+  //update current value
+  const transactionResponse = await simpleStorage.store(7)
+  await transactionResponse.wait(1)
+  const updatedValue = await simpleStorage.retrieve()
+  console.log(`Updated Value is: ${updatedValue}`)
 }
 
 async function verify(contractAddress, args) {
